313b4dbcedc0da3b231e974155ae68c05086db39
Linden <lindenpowell218@gmail.com>
2025-04-07 23:39:40 +0800

    I added comments.

--- Quiz_creator.py
+++ Quiz_creator.py
@@ -1,7 +1,9 @@
 # Create a program that makes a quiz, it includes questions, multiple choices, and scoring.
-
+# List of questions
 questions = []
 print("Enter as many questions with multiple choices as you want: ")
+
+# While loop: "Ask another question until the user chose to exit" as per sir's instructions.
 while True:
     question = input(f"Enter a question: ")
     choice_a = input("Choice A: ")
@@ -9,7 +11,11 @@ while True:
     choice_c = input("Choice C: ")
     choice_d = input("Choice D: ")
 
+# Asks for the correct answer among the choices.
     answer = input("What is the correct answer? (A, B, C OR D): ").upper()
+
+# .append adds the inputted data into the list.
+# Which is probably going to be important later on, based on how I plan to make this.
     questions.append({f"question: ": question,
                       "choices: ": {"A": choice_a,
                                     "B": choice_b,
