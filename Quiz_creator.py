7f9068a5f6908d0bb71ab4e95e0338b30fcecc6f
Linden <lindenpowell218@gmail.com>
2025-04-07 23:06:06 +0800

    I added a while loop, it follows the instruction sir have given us in which it will only stop only if the userwants to. In the loop, it includes the input var like question1, 2, 3 along with its options. The questions = [], is there bc I plan to add every input inside the list.

---
+++ Quiz_creator.py
@@ -0,0 +1,12 @@
+# Create a program that makes a quiz, it includes questions, multiple choices, and scoring.
+
+questions = []
+print("Enter as many questions with multiple choices as you want: ")
+while True:
+    question = input(f"Enter a question: ")
+    choice_a = input("Choice A: ")
+    choice_b = input("Choice B: ")
+    choice_c = input("Choice C: ")
+    choice_d = input("Choice D: ")
+
+
