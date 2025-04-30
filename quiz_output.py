7e140adaa23df4de7fd5f30bb4b3430b8cd7f51d
Linden <lindenpowell218@gmail.com>
2025-04-30 15:06:20 +0800

    added random.shuffle, because it is required for the quiz to be randomized

--- quiz_output.py
+++ quiz_output.py
@@ -1,5 +1,9 @@
 # 1. Read collected_data.txt
 #
+
+import random
 with open("collected_data.txt", "r") as quiz:
-    to_answer = quiz.read()
+    to_answer = quiz.readlines()
+
+random.shuffle(to_answer)
 print(to_answer)
