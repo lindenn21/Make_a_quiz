d848f797885d1b2a649c41a1b36ba4c96930721f
Linden <lindenpowell218@gmail.com>
2025-04-30 21:52:21 +0800

    While loop added, also changed some of the variable name to match the content

--- quiz_output.py
+++ quiz_output.py
@@ -1,9 +1,15 @@
 # 1. Read collected_data.txt
-#
+# 2. Randomize the lines
 
 import random
-with open("collected_data.txt", "r") as quiz:
-    to_answer = quiz.readlines()
 
-random.shuffle(to_answer)
-print(to_answer)
+# Opens the txt file with inputted questions and answers
+with open("collected_data.txt", "r") as the_quiz:
+    quiz_content = the_quiz.readlines()
+
+random.shuffle(quiz_content)
+
+quiz = []
+num = 0
+while num < len(quiz_content):
+
