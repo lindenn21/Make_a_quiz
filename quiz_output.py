dd9feb26bab4f4b008ddb683d43cd32bc282f55e
Linden <lindenpowell218@gmail.com>
2025-04-30 22:08:34 +0800

    Removed import random, and shuffle, I decided to use it once the setup of the quiz is done.

--- quiz_output.py
+++ quiz_output.py
@@ -1,15 +1,16 @@
 # 1. Read collected_data.txt
 # 2. Randomize the lines
 
-import random
-
 # Opens the txt file with inputted questions and answers
 with open("collected_data.txt", "r") as the_quiz:
     quiz_content = the_quiz.readlines()
 
-random.shuffle(quiz_content)
-
+# This will put everything together once I append.
 quiz = []
 num = 0
 while num < len(quiz_content):
+
+
+
+
 
