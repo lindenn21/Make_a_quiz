9355e50a508390498c486b5ab05299161f07f55b
Linden <lindenpowell218@gmail.com>
2025-05-02 17:23:28 +0800

    Imported random, this randomizes the question that will appear.

--- quiz_output.py
+++ quiz_output.py
@@ -1,6 +1,7 @@
 # 1. Read collected_data.txt
 # 2. Randomize the lines
 
+import random
 # Opens the txt file with inputted questions and answers
 with open("collected_data.txt", "r") as the_quiz:
     quiz_content = the_quiz.readlines()
@@ -29,6 +30,7 @@ while num < len(quiz_content):
     else:
         num += 1
 
+random.shuffle(quiz)
 
 score = 0
 question_num = 1
